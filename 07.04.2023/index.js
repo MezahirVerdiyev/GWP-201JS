dcda5613015a4cc126743111502005ebd9dc8292
Mezahirverdiyev <verdiyevmezahir25@gmail.com>
2023-04-09 12:07:47 +0400

    js

---
+++ 07.04.2023/index.js
@@ -0,0 +1,79 @@
+
+
+// 3.Sum of even numbers(e.g:2,4,6...) in 10
+// let sum=0
+// for(i=0;i<10;i=i+2){
+//     sum+=i
+// }console.log(sum)
+
+
+
+// 4.Multiply the odd SVGAnimatedNumberList(e.g;1,3,5...)in 17
+// let sum=1
+// for(i=1;i<17;i=i+2){
+//     sum*=i
+// }console.log(sum)
+
+
+// // 4.Multiply numbers beetwen 10 and 18
+// let sum=10
+// for(i=10;i<=18;i++){
+//     sum*=i
+// }console.log(sum)
+
+// // 5.Sum numbers betwen 17 and 30
+// let sum=0
+// for(i=17;i<=30;i++){    
+//     if(i==20 ){
+//         continue    
+//     }
+//     if(i==19 ){
+//         continue    
+//     }
+//     sum+=i    
+// }console.log(sum)
+
+// 7.Find of even SVGAnimatedNumberList(e.g;2,4,6..) in 16. And every number to the power of 2.
+// for(i=0;i<=16;i++){ 
+//       console.log(2**i ,`2nin quvvetidir`)
+//      }
+
+// 6.Sum of even numbers and assign this result a variable.
+// Sum of odd numbers and assign this result a variable.
+
+// let even=0
+// let odd=1
+// let i=1
+// while(i<=30){
+//     if(i%2==0){
+//        console.log(i, `even`) 
+//     }else{
+        
+//         console.log(i, `odd`)
+//     }
+//     i++
+// }
+
+
+
+
+
+
+
+
+
+
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+
