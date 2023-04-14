f5e71eb4f65ccafdc03993ddacc42fa5314a45b3
Mezahirverdiyev <verdiyevmezahir25@gmail.com>
2023-04-14 10:21:00 +0400

    15.FunctionJS

---
+++ 15.Function/script.js
@@ -0,0 +1,77 @@
+///////1
+// function getFirstValue(arr=[a,b,c]){
+//     return arr[0]
+// }
+// console.log(getFirstValue([1,2,3]))
+// console.log(getFirstValue([80,5,100]) )
+// console.log(getFirstValue([-500,0,50]))
+
+
+// ///////2
+
+// function getLAstItem(arr1=[a,b,c]){
+//     return arr1.at(-1)
+// }console.log(getLAstItem([1,2,3]))
+// console.log(getLAstItem(['cat','dog','duck']))
+// console.log(getLAstItem([true,false,true]))
+
+
+// ///////3
+
+
+// ///////4
+// let sum=0
+// function addition(a,b){
+//     sum=a+b
+//     return sum
+// }console.log(addition(3,2))
+// console.log(addition(-3,-6))
+// console.log(addition(7,3))
+
+
+//////5
+// let sum4=0
+// function lessThan100(a,b){
+    
+//     sum4=a+b
+//     if(sum4>100){
+//         console.log(false)
+//     }else{
+//         console.log(true)
+//     }
+//     return sum4
+    
+// }
+// console.log(lessThan100(22,15))
+// console.log(lessThan100(83,34))
+// console.log(lessThan100(3,77))
+
+
+
+
+// //////6
+// let sum1=1
+// function volume0fBox(width,length,height){
+//     sum1=width*length*height
+//     return sum1
+// }console.log(volume0fBox(2,5,1))
+// console.log(volume0fBox(4,2,2))
+// console.log(volume0fBox(2,3,5))
+
+
+// //////7
+// let sum2=0
+// function points(twoPointes,threePointers){
+//     sum2=2*twoPointes+3*threePointers
+//     return sum2
+// }console.log(points(1,1))
+// console.log(points(7,5))
+// console.log(points(38,8))
+
+/////8
+
+
+
+
+
+
